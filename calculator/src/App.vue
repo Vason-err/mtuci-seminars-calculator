ec4bb73edb48f506ee5ba423cabc9aa11c83ff26
Vason-err <vasya.vedan@gmail.com>
2022-04-30 12:01:45 +0300

    Simplest calculator Vue.js

---
+++ calculator/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <CalculatorActions/>
+  </div>
+</template>
+
+<script>
+import CalculatorActions from "@/components/CalculatorActions";
+
+export default {
+  name: 'App',
+  components: {
+    CalculatorActions,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
